be64c462318acbd2b5713e4e87678b51fd0746a8
sivasomanath2502 <gsomanath2502@gmail.com>
2025-10-20 16:31:26 +0530

    Test Program Added

---
+++ src/file_test.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+int main()
+{
+	int fd = open("data/accounts.data",O_RDWR | O_CREAT,06444);
+	if(fd < 0)
+	{
+		perror("open");
+		return 1;
+	}
+
+	char record[] = "1001,5000.00\n";
+	write(fd,record,strlen(record));
+
+	lseek(fd,0,SEEK_SET);
+	char buf[64];
+	int n = read(fd,buf,sizeof(buf)-1);
+	buf[n] = '\0';
+
+	printf("File Contents:\n%s\n",buf);
+	close(fd);
+	return 0;
+}
